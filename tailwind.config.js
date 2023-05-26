992db5ee82bac0d34ccaacd9f8850275abd71df3
lin.wang <mag1c_shen_09@foxmail.com>
2023-05-26 15:11:57 +0800

    feat: header component & page background image linear-gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 const path = require("path");
 
-const spacing = Array(100)
+const spacing = Array(200)
   .fill("$")
   .map((_, index) => index + 1)
   .reduce((spacing, index) => {
-    spacing[index] = `${index / 4}rem`;
+    spacing[index / 2] = `${index / 4 / 2}rem`;
     return spacing;
   }, {});
 
 module.exports = {
-  content: [path.resolve(__dirname, "./src/**/*.{js,ts,jsx,tsx}")],
+  content: [
+    path.resolve(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
+    path.resolve(__dirname, "./index.html"),
+  ],
   theme: {
     extend: {
       spacing,
