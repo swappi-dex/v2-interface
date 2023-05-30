4063a98afb527b973bb111fd54c79309d12cf277
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-05-30 18:08:09 +0800

    chore: Use unocss instead of tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'uno.css';
+import '@unocss/reset/tailwind.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Unocss from "unocss/vite";
+import presetWind from "@unocss/preset-wind";
+import transformerDirective from "@unocss/transformer-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    Unocss({
+      transformers: [transformerDirective()],
+      presets: [presetWind()],
+      theme: {
+        lineHeight: {
+          inherit: "inherit",
+        },
+        colors: {
+          "ink-green": "#2A3D4A",
+          "ink-gray": "#AAB1B7",
+          grass: "#D9ECCA",
+          windy: "#C8E3B3",
+          orange: "#EBA337",
+          lime: "#BEE09C",
+          bluegreen: "#4DAF9E",
+          medigreen: "#163B56",
+          limo: "#A6D379",
+          pinko: "#FEE9E8",
+          "activate-green": "#7fbf4e",
+          fail: "#D96E81",
+          "light-black": "#474C5F",
+          diary: "#864551",
+          brownie: "#4A2A41",
+        },
+        boxShadow: {
+          popper: "0px 4px 16px rgba(0,0,0,0.16)",
+        },
+      },
+    }),
+    react(),
+  ],
+});
